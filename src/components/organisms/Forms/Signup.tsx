36a3904071503af5eace45593aaebe03adec2d2d
Angelo Zambrano <angelozam17@gmail.com>
2021-01-04 19:00:02 -0500

    fix: clean signup form

--- src/components/organisms/Forms/Signup.tsx
+++ src/components/organisms/Forms/Signup.tsx
@@ -121,9 +121,6 @@ const Login = (): JSX.Element => {
                <Transition visible={!!error} animation="shake" duration={500}>
                   <Message
                      error
-                     header={`${error?.response?.data.error || 'Error'}: ${
-                        error?.response?.data.statusCode || 500
-                     }`}
                      content={handleErrors(error?.response?.data.message || [])}
                   />
                </Transition>
